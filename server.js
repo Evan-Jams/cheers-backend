ec627c10ae88ffbb58b32a5f9c6c0708a8778168
John Hitz <johnh@whodapet.com>
2020-03-17 10:17:44 -0500

    Added cors middleware

--- server.js
+++ server.js
@@ -3,16 +3,30 @@
 ******************************************************************************/
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3003
 const mongoose = require('mongoose')
 const beerController = require('./controllers/beers.js')
+
 
 /*****************************************************************************
                               MIDDLEWARE
 ******************************************************************************/
 
 app.use(express.json())
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 /******************************************************************************
                         MONGOOSE CONNECTION
